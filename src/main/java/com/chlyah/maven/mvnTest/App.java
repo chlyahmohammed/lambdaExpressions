efbd64798b6ec8bd678f923ce49164f9fcb6212e
chlyahmohammed <chlyahmohammed@gmail.com>
2019-12-22 17:08:16 +0100

    First real commit

---
+++ src/main/java/com/chlyah/maven/mvnTest/App.java
@@ -0,0 +1,13 @@
+package com.chlyah.maven.mvnTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
